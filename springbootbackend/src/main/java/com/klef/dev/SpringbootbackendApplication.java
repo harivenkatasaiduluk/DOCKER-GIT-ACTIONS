23f2178a311276d5ded93bd6d72ea86e7992f698
KHVS <2300032396@kluniversity.in>
2025-10-23 20:21:21 +0530

    Full dtsck v2.0

--- springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -2,10 +2,9 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootbackendApplication extends SpringBootServletInitializer
+public class SpringbootbackendApplication
 {
 	public static void main(String[] args) 
 	{
